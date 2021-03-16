a98d494cb768416c2e608818185cb0730dab8706
inesBenS <ines.ben-slama@isen.yncrea.fr>
2021-03-16 17:15:56 +0100

    initialisation des routes et création de certains components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view.component';
+import { PostItemComponent } from './post-item/post-item.component';
+import { NewPostComponent } from './new-post/new-post.component';
+import { ListPostComponent } from './list-post/list-post.component';
+
+// Routing 
+import { RouterModule, Routes } from '@angular/router';
+
+
+// Création des différentes routes de l'application
+const appRoutes: Routes = [
+  { path: 'Home', component: ViewComponent },
+  { path: 'Home/:post', component: PostItemComponent },
+  { path: 'NewPost', component: NewPostComponent},
+  { path: '', component: ViewComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    PostItemComponent,
+    NewPostComponent,
+    ListPostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
